52be72e8af775a487f57d6481b4a9785f353469c
FrancescoSls97 <frankds1997@gmail.com>
2025-07-29 13:04:09 +0200

    search button funzionante

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
 import { useState } from 'react'
 
 
+
+
 function App() {
   
+  const [films, setFilms] =  useState([])
+  const [searchTerms, setSearchTerms] = useState('')
+  
+  const handleSearch = () => {
+    if (searchTerms.trim() === '') {
+      setFilms([]);
+      return;
+    }
+  
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=53633f25df0de94ef08fd28009d6d9e5
+&query=${searchTerms}`)
+    .then(res => res.json())
+    .then(data =>{
+      console.log(data.results); 
+      setFilms(data.results) 
+    })
+    .catch(error => {
+      console.error('Errore durante la ricerca', error);
+      
+    });
+  };
 
   return (
     <>
     <header>
-    <nav class="navbar bg-body-tertiary">
-     <div class="container">
+    <nav className="navbar bg-body-tertiary">
+     <div className="container">
       <a href="https://fontmeme.com/fonts/one-piece-manga-font/"><img src="https://fontmeme.com/permalink/250729/f31b4a3dfbe193545ccbe23920ec7db0.png" alt="one-piece-manga-font" border="0"/></a>
         </div>
       </nav>
@@ -17,9 +40,18 @@ function App() {
       <div className="section m-5">
         <div className="container">
           <div className="form">
-            <input className="form-control-lg m-2" type="text" placeholder='Cerca un film'/>
-            <a class="btn btn-primary btn-lg " href="#" role="button">Cerca</a>
+            <input className="form-control-lg m-2" type="text" value={searchTerms} onChange={(e) =>  setSearchTerms(e.target.value)} placeholder='Cerca un film'/>
+            <button className="btn btn-primary btn-lg " href="#" role="button" onClick={handleSearch}>Cerca</button>
           </div>
+          <h2>Risultati</h2>
+          {films.map(film => (
+            <div className='results' key={film.id}>
+              <h3>{film.title}</h3>
+              <h5 className="fs3">Titolo Originale: {film.original_title}</h5>
+              <p>Lingua: {film.original_language}</p>
+              <p>Voto: {film.vote}</p>
+            </div>
+          ))}
         </div>
       </div>
     </main>
